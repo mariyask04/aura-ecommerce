640d9e8f48e9fe84d8a41394a48d168d14eae06b
mariyask04 <skmariya484@gmail.com>
2026-06-08 07:37:41 +0530

    Fix cors roigin url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000","https://aura-ecommerce-tan.vercel.app/"]
+    origin:["http://localhost:3000","https://aura-ecommerce-tan.vercel.app"]
 }));
 
 app.use("/api/auth", authRoutes);
